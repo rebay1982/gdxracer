980512e834116c1a1681d50a00fe615ff5e6a6f3
Reda Baydoun <reda.baydoun@gmail.com>
2018-09-16 23:16:10 -0400

    Initial project

---
+++ desktop/src/com/redbay/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.redbay.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.redbay.game.GdxRacer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxRacer(), config);
+	}
+}
